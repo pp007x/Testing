669e87b1553074ceef73d69262fad1aaf536a9ec
StephanKragten <72765452+pp007x@users.noreply.github.com>
2023-07-25 05:24:36 +0200

    Dox

--- Backend/Controllers/OpenReactionForm.cs
+++ Backend/Controllers/OpenReactionForm.cs
@@ -4,8 +4,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
-// using DocumentFormat.OpenXml.Packaging;
-// using DocumentFormat.OpenXml.Wordprocessing;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 using System.Xml.Linq;
 // using NPOI.XWPF.UserModel;
 // using DocumentFormat.OpenXml.Packaging;
@@ -264,21 +264,21 @@ namespace LoginApi.Controllers
         }
 
 
-// public void UpdateWordDocument(string filePath, string newFilePath, Dictionary<string, string> replacements)
-// {
-//     // Copy the original document to a new file
-//     System.IO.File.Copy(filePath, newFilePath, true);
+public void UpdateWordDocument(string filePath, string newFilePath, Dictionary<string, string> replacements)
+{
+    // Copy the original document to a new file
+    System.IO.File.Copy(filePath, newFilePath, true);
 
-//     // Now open and modify the copy, not the original
-//     using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(newFilePath, true))
-//     {
-//         foreach (var replacement in replacements)
-//         {
-//             // Perform the replacements on the document
-//             TextReplacer.SearchAndReplace(wordDoc, replacement.Key, replacement.Value, false);
-//         }
-//     }
-// }
+    // Now open and modify the copy, not the original
+    using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(newFilePath, true))
+    {
+        foreach (var replacement in replacements)
+        {
+            // Perform the replacements on the document
+            // TextReplacer.SearchAndReplace(wordDoc, replacement.Key, replacement.Value, false);
+        }
+    }
+}
 
 
 [HttpGet("updatedWordDocument")]
